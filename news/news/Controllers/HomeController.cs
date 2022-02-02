b5b769e69cef7887cefc06842b05fa254cd442d7
ahmedelgarf94 <ahmedelgarf94@gmail.com>
2022-02-02 19:25:48 +0200

    Added ContactUs page
    changed time to be current time and added autocomplete for the model and added contact us page

--- news/news/Controllers/HomeController.cs
+++ news/news/Controllers/HomeController.cs
@@ -35,6 +35,10 @@ namespace news.Controllers
         {
             return View();
         }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
